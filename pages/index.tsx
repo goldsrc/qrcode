42d9521ad16ad2c96eaee2b35c0c072ac53e854a
Mohammad Beheshti <mbeheshtis@gmail.com>
2021-11-15 23:30:33 +0330

    feat: download button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import React from "react";
+import { useCallback, useRef } from "react";
 import { Form, Field } from "react-final-form";
 import QRCode from "qrcode.react";
 
 const Home: NextPage = () => {
+  const onDownload = useCallback(() => {
+    var link = document.createElement("a");
+    link.download = "qrcode.png";
+    link.href = (
+      document.getElementById("code") as HTMLCanvasElement
+    ).toDataURL();
+    link.click();
+  }, []);
   return (
     <>
       <Head>
@@ -27,14 +35,24 @@ const Home: NextPage = () => {
                 name="text"
                 id="text"
                 className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                defaultValue={""}
+                defaultValue=""
               />
             </div>
             <QRCode
+              id="code"
               className="mt-2 mx-auto"
+              size={256}
               level="H"
+              renderAs="canvas"
               value={values.text || ""}
             />
+            <button
+              onClick={onDownload}
+              type="button"
+              className="mx-auto my-5 flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Download
+            </button>
           </form>
         )}
       />
